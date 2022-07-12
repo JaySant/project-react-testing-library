67334ecf8e50279f2c379ce7d104e9af091b39d6
Jay Santos <jaysant15@gmail.com>
2022-07-12 00:58:38 -0300

    Requisito 1 - completo

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,40 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente <App.js />', () => {
+  it('Verifica se ao clicar em "Home" é direcionado para a pagina inicial', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const homeElement = screen.getByRole('link', { name: 'Home' });
+    expect(homeElement).toBeInTheDocument();
+    userEvent.click(homeElement);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Verifica se ao clicar em "About" é direcionado para a pagina about', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const aboutElement = screen.getByRole('link', { name: 'About' });
+    expect(aboutElement).toBeInTheDocument();
+    userEvent.click(aboutElement);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('Verifica se ao clicar em "Favorite Pokemons" é direcionado para favoritos', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const favoriteElement = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(favoriteElement).toBeInTheDocument();
+    userEvent.click(favoriteElement);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+});
