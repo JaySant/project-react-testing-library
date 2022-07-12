eae7f1a77815de4fc212b5f05e43e1df0c364f1f
Jay Santos <jaysant15@gmail.com>
2022-07-12 03:58:27 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import { FavoritePokemons } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+// import App from '../App';
+
+describe('Testa o componente <FavoritePokemons.js />', () => {
+  it('Teste se é exibida na tela a mensagem "No favorite pokemon found"', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const notFoundFavorite = screen.getByText(/No favorite pokemon found/i);
+    expect(notFoundFavorite).toBeInTheDocument();
+  });
+});
